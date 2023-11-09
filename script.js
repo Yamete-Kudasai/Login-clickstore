f05f55ab728fd47448905e332918f1cf5fecfd77
Yamete-Kudasai <grisalesmith@gmail.com>
2023-11-08 19:55:09 -0500

    subizado del menu movil y rotar boton de menu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const botonmenu=document.querySelector(".icon-menu");
 const listamenu=document.querySelector(".menu ul");
+const botonmenurotar=document.querySelector(".icon-menu img");
 
 botonmenu=addEventListener("click",()=>{
     listamenu.classList.toggle("vistamenu");
-
+    botonmenurotar.classList.toggle("rotar");
 });
+
