5b2d1f766e5e0d97c30cc017c0f37f1888c6d203
Yamete-Kudasai <grisalesmith@gmail.com>
2023-11-08 19:38:38 -0500

    creacion de menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const botonmenu=document.querySelector(".icon-menu");
+const listamenu=document.querySelector(".menu ul");
+
+botonmenu=addEventListener("click",()=>{
+    listamenu.classList.toggle("vistamenu");
+
+});
